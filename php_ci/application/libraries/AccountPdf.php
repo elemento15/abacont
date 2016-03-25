857d37a3aee1a360fc2a1b73f39f53c753c0ee29
Luis Lomeli <luism.lomeli@gmail.com>
2016-03-24 20:49:30 -0700

    Buttons template to Forms

---
+++ php_ci/application/libraries/AccountPdf.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class AccountPdf {
+
+    public function some_function() {
+    }
+}
+
+/* End of file AccountPdf.php */
