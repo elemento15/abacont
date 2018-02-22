3931b4c79cf135f97a0951b58f3a39530e6dfd4c
Luis Lomeli <luism.lomeli@gmail.com>
2018-02-22 00:47:43 -0700

    Improving data results in charts

--- php_ci/application/controllers/Charts.php
+++ php_ci/application/controllers/Charts.php
@@ -17,16 +17,35 @@ class Charts extends CI_Controller {
 	}
 
 	public function expenses_day() {
+		$data          = [];
 		$month         = $_POST['month'];
 		$year          = intval($_POST['year']);
 		$category      = intval($_POST['category']);
 		$subcategory   = intval($_POST['subcategory']);
 		
-		$data = $this->modelMov->movs_grouped('D', 'G', $month, $year, $category, $subcategory);
+		$records = $this->modelMov->movs_grouped('D', 'G', $month, $year, $category, $subcategory);
+		
+
+		// create list of days in the period
+		$data = $this->getListDaysInMonth($year, $month);
+
+		foreach ($data as $index => $item) {
+			$data[$index]['total'] = 0;
+
+			foreach ($records as $rec) {
+				if ($rec->fecha == $item['fecha']) {
+					$data[$index]['total'] = $rec->total;
+				}
+			}
+		}
+
 		echo json_encode($data);
 	}
 
 	public function expenses_months() {
+		$months = 24;
+		$data = [];
+
 		$category      = intval($_POST['category']);
 		$subcategory   = intval($_POST['subcategory']);
 
@@ -42,13 +61,30 @@ class Charts extends CI_Controller {
 			WHERE mov.tipo = 'G' AND NOT mov.cancelado $filters 
 			GROUP BY mov_fecha 
 			ORDER BY mov_fecha DESC 
-			LIMIT 24;");
+			LIMIT $months ;");
 
-		$data = $query->result_array();
+		$records = $query->result_array();
+
+		// create list of months in the period
+		$data = $this->getListPastMonths($months);
+
+		foreach ($data as $index => $item) {
+			$data[$index]['total'] = 0;
+
+			foreach ($records as $rec) {
+				if ($rec['mov_fecha'] == $item['mov_fecha']) {
+					$data[$index]['total'] = $rec['total'];
+				}
+			}
+		}
+
 		echo json_encode($data);
 	}
 
 	public function average_months() {
+		$months = 24;
+		$data = [];
+
 		$category      = intval($_POST['category']);
 		$subcategory   = intval($_POST['subcategory']);
 
@@ -65,23 +101,68 @@ class Charts extends CI_Controller {
 			WHERE mov.tipo = 'G' AND NOT mov.cancelado $filters 
 			GROUP BY mov_fecha 
 			ORDER BY mov_fecha DESC 
-			LIMIT 24;");
+			LIMIT $months ;");
 
-		$data = $query->result_array();
+		$records = $query->result_array();
 
-		foreach ($data as $key => $item) {
-			$date = explode('-', $item['mov_fecha']);
 
-			// on current month, set current days
-			if (date('Y') == $date[0] && date('m') == $date[1]) {
-				$days = date('d');
-			} else {
-				$days = cal_days_in_month(CAL_GREGORIAN, $date[1], $date[0]);
+		// create list of months in the period
+		$data = $this->getListPastMonths($months);
+
+		foreach ($data as $index => $item) {
+			$data[$index]['total'] = 0;
+
+			foreach ($records as $rec) {
+				if ($rec['mov_fecha'] == $item['mov_fecha']) {
+					
+					$date = explode('-', $rec['mov_fecha']);
+
+					// on current month, set current days
+					if (date('Y') == $date[0] && date('m') == $date[1]) {
+						$days = date('d');
+					} else {
+						$days = cal_days_in_month(CAL_GREGORIAN, $date[1], $date[0]);
+					}
+
+					$data[$index]['total'] = round($rec['sum_importe'] / $days, 2);
+				}
 			}
-
-			$data[$key]['total'] = round($item['sum_importe'] / $days, 2);
 		}
 
 		echo json_encode($data);
+	}
+
+
+	protected function getListPastMonths($months) {
+		$date = new DateTime();
+		$list = array( array('mov_fecha' => $date->format('Y-m')) );
+
+		for ($i=0; $i < $months - 1; $i++) { 
+			$list[] = array('mov_fecha' => $date->sub(date_interval_create_from_date_string('1 month'))->format('Y-m'));
+		}
+
+		return $list;
+	}
+
+	protected function getListDaysInMonth($year, $month) {
+		$list = array();
+
+		if ($month) {
+			$days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+			
+			for ($i=1; $i <= $days ; $i++) {
+				$txt_month = str_pad($month, 2, '0', STR_PAD_LEFT);
+				$txt_day = str_pad($i, 2, '0', STR_PAD_LEFT);
+				$list[] = array('fecha' => $year.'-'.$txt_month.'-'.$txt_day);
+			}
+
+		} else {
+			for ($i=1; $i <= 12 ; $i++) { 
+				$arr_days = $this->getListDaysInMonth($year, $i);
+				$list = array_merge($list, $arr_days);
+			}
+		}
+
+		return $list;
 	}
 }
