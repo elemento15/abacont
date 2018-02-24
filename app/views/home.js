ba7cd1e3c5958c59178c3a4bbfda588740012a0f
Luis Lomeli <luism.lomeli@gmail.com>
2018-02-24 00:38:24 -0700

    Adding KPIS to main screen

--- app/views/home.js
+++ app/views/home.js
@@ -4,13 +4,34 @@ define(function (require) {
 
   var Backbone = require('backbone'),
     tpl      = require('text!tpl/home.htm'),
-    template = _.template(tpl);
+    template = _.template(tpl),
+    Defaults = require('app/defaults');
 
   return Backbone.View.extend({
     render: function () {
       this.$el.html(template());
       return this;
+    },
+
+    onRender: function () {
+      this.getKPIData(); 
+    },
+
+    getKPIData: function () {
+      $.when(
+        $.ajax({
+          url: Defaults.ROUTE + 'main/kpis',
+          type: 'POST',
+          dataType: 'json'
+        })
+      ).then(function (data, textStatus, jqXHR) {
+        $('#kpiIng30d').html('$'+(data.ing30d || 0).formatMoney());
+        $('#kpiExp30d').html('$'+(data.exp30d || 0).formatMoney());
+        $('#kpiIng6m').html('$'+(data.ing6m || 0).formatMoney());
+        $('#kpiExp6m').html('$'+(data.exp6m || 0).formatMoney());
+      });
     }
+
   });
 
 });
--- php_ci/application/controllers/Main.php
+++ php_ci/application/controllers/Main.php
@@ -44,4 +44,37 @@ class Main extends CI_Controller {
     	session_destroy();
     	echo json_encode(array('success' => true));
 	}
+
+	public function kpis() {
+		// get dates for 30 days and 6 months
+		$query = $this->db->query("
+			select DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 30 DAY), '%Y-%m-%d') AS date30d, 
+			       DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 6 MONTH), '%Y-%m-%d') AS date6m, 
+			       DATEDIFF(NOW(), DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 6 MONTH), '%Y-%m-%d')) AS days6m;");
+
+		$res = $query->result_array();
+		$dates = $res[0];
+
+		// get data
+		$query = $this->db->query("
+			select (SELECT SUM(importe) 
+			        FROM movimientos 
+			        WHERE fecha BETWEEN '".$dates['date30d']."' AND NOW() AND 
+			              tipo = 'I' AND NOT cancelado) / 30 AS ing30d, 
+			       (SELECT SUM(importe) 
+			        FROM movimientos 
+			        WHERE fecha BETWEEN '".$dates['date30d']."' AND NOW() AND 
+			              tipo = 'G' AND NOT cancelado) / 30 AS exp30d, 
+			       (SELECT SUM(importe) 
+			        FROM movimientos 
+			        WHERE fecha BETWEEN '".$dates['date6m']."' AND NOW() AND 
+			              tipo = 'I' AND NOT cancelado) / ".$dates['days6m']." AS ing6m, 
+			       (SELECT SUM(importe) 
+			        FROM movimientos 
+			        WHERE fecha BETWEEN '".$dates['date6m']."' AND NOW() AND 
+			              tipo = 'G' AND NOT cancelado) / ".$dates['days6m']." AS exp6m;");
+
+		$data = $query->result_array();
+		echo json_encode($data[0]);
+	}
 }
