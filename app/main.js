7cd6b0f01716dae5a92c661297a94eb61d9701f3
Luis Lomeli <luism.lomeli@gmail.com>
2023-07-29 11:28:50 -0700

    Improve accounts and movements index views

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 require.config({
   baseUrl: 'libs',
-  urlArgs: "v=3.21",
+  urlArgs: "v=3.22",
   
   // uncomment in development mode, avoid cached JS
   //urlArgs: "v=" + (new Date()).getTime(),
--- php_ci/application/views/main.php
+++ php_ci/application/views/main.php
@@ -10,10 +10,10 @@
     <!-- styles -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="css/bootstrap-datepicker.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/styles.css?v=4.2" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css?v=4.3" />
     
     <!-- main require module -->
-    <script data-main="app/main.js?v=3.20" src="libs/require.js"></script>
+    <script data-main="app/main.js?v=3.22" src="libs/require.js"></script>
   </head>
   <body></body>
 </html>
