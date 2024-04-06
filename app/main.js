fb99d14136b2f360eb9c81127aeb636386a2495d
Luis Lomeli <luism.lomeli@gmail.com>
2024-04-06 09:54:54 -0700

    increase js version

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 require.config({
   baseUrl: 'libs',
-  urlArgs: "v=3.30",
+  urlArgs: "v=3.31",
   
   // uncomment in development mode, avoid cached JS
   //urlArgs: "v=" + (new Date()).getTime(),
